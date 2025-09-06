10e526befa51bf1204080a97ea660bba7a7c735c
KASA <kasasachith@gmail.com>
2025-09-06 17:05:43 +0530

    commiting data types

---
+++ 01_dataTypes/sample.js
@@ -0,0 +1,13 @@
+// The primitive data types in JAVA SCRIPT are:
+// 1.Number
+// 2.string
+// 3.boolean
+// 4.undefined
+// 5.null
+// 6.bigint
+// 7.symbol
+
+
+console.log(typeof(1))
+
+console.log(typeof(true))
