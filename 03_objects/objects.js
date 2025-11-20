abd35870c6a28d8408624df5600e64a07ffc865c
KASA <kasasachith@gmail.com>
2025-11-20 08:58:08 +0530

    written the topicsList of objects in objects topics.md

--- 03_objects/objects.js
+++ 03_objects/objects.js
@@ -11,41 +11,41 @@ const aPerson = {
         state: "NY"
     },
 }
-// console.log(aPerson);
+console.log(aPerson);
 
 //to access the data from object we can use dot notation or bracket notation
-// console.log(aPerson.name);
-// console.log(aPerson["age"]);
+console.log(aPerson.name);
+console.log(aPerson["age"]);
 
 //to add a new property to an object
 aPerson.email = "john@gmail.com"
-// console.log(aPerson);
+console.log(aPerson);
 
 //to delete a property from an object
-// delete aPerson.isMarried;
+delete aPerson.isMarried;
 console.log(aPerson);
 
 //to loop through an object we can use for in loop
 for (let key in aPerson) {
-    // console.log(key, ":", aPerson[key]);
+    console.log(key, ":", aPerson[key]);
 }
 
 //to get all the keys of an object we can use Object.keys() method
 const keys = Object.keys(aPerson);
-// console.log(keys);
+console.log(keys);
 
 //to get all the values of an object we can use Object.values() method
 const values = Object.values(aPerson);
-// console.log(values);
+console.log(values);
 
 //to get all the entries of an object we can use Object.entries() method
 //we use this entries method to convert object into array of arrays and access both key and value at the same time.
 const entries = Object.entries(aPerson);
 console.log("Entries:");
-console.log(entries);   
+console.log(entries);
 
 //to check if a property exists in an object we can use hasOwnProperty() method
-// console.log(aPerson.hasOwnProperty("name")); //true
+console.log(aPerson.hasOwnProperty("name")); //true
 
 //to merge two objects we can use Object.assign() method
 const anotherPerson = {
---
+++ 03_objects/topics.md
@@ -0,0 +1,14 @@
+# Objects: 
+1. creating an object using {}
+2. Accessing through 
+    - .(DOT NOTATION)
+    - [](BRACES NOTATION)
+3. Adding a new property to the object
+4. Delete a property from the object
+5. Looping the object
+6. Getting the **KEYS** only
+7. Getting the **VALUES** only
+8. Getting the **KEY-VALUES(ENTRIES)** only
+9. Checking a property is exist in object or not using **hasOwnProperty()**
+10. Assigning the current object to another object using **Object.assign()**
+11. Getting the number of properities of an object using **Object.keys(objectName).length**
