0eb8d2d282b6fb966af3c1a798ed37a769c6a194
KASA <kasasachith@gmail.com>
2025-11-21 12:08:47 +0530

    completed the dialog box module

--- 03_dialogBoxes/dialogBox.js
+++ 03_dialogBoxes/dialogBox.js
@@ -1,16 +1,17 @@
 // about the dialog box
 
-// alert box
+// alert box: which only shows a message
 alert("This is an alert box!");
 
-// confirm box
+// confirm box: which returns true or false
 let userResponse = confirm("Do you want to proceed?");
 if (userResponse) {
     console.log("User chose to proceed.");
 } else {
     console.log("User canceled the action.");
 }
-// prompt box
+
+// prompt box: which asks for user input and returns the input value which is a string
 let userName = prompt("Please enter your name:", "Guest");
 if (userName !== null) {
     console.log("Hello, " + userName + "!");
